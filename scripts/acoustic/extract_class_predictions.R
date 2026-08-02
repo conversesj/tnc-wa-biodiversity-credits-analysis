32c8a3e0fbead6197b300436049be957ea4be4cf
Jacqueline Jiaying Wu <159320029+JacW27@users.noreply.github.com>
2026-08-01 19:15:40 -0700

    Update focal classes and filter column names in R script

--- scripts/acoustic/extract_class_predictions.R
+++ scripts/acoustic/extract_class_predictions.R
@@ -3,7 +3,7 @@
 # audio data to derive species- and model-specific segments via the GUI from Jacuzzi and Olden 2025.
 #
 # CONFIG:
-focal_classes = c("olive-sided flycatcher", "hairy woodpecker")
+focal_classes = c("Olive-sided Flycatcher", "Hairy Woodpecker")
 threshold = 0.99
 #
 # OUTPUT:
@@ -11,21 +11,24 @@ path_out = "data/debug/helper_get_class_predictions/" # TODO: Change filepath to
 #
 # INPUT:
 path_prediction_data = "data/cache/1_pam/2_agg_raw_predictions/NEW_prediction_data.feather" # TODO: Change filepath to point to your prediction data
+# path_prediction_data = "filename.xlsx"
 ####################################################################################################
 
 library(dplyr)
+library(readxl)
 library(readr)
 library(purrr)
 library(crayon)
 
 message("Loading all prediction data")
 pred_data = read_feather(path_prediction_data) # TODO: Read all your prediction data as a tibble
+# pred_data = read_excel(path_prediction_data)
 
 for (focal_class in focal_classes) {
   
   # Filter for class predictions
   class_data = pred_data %>%
-    filter(common_name == focal_class, confidence_target > threshold) # TODO: Filter according to the model you want to use
+    filter(`Common name` == focal_class, Confidence_target > threshold) # TODO: Filter according to the model you want to use
   message("Found ", nrow(class_data), " predictions for class '", focal_class, "' using threshold ", threshold)
   
   # Store associated audio file path and name
